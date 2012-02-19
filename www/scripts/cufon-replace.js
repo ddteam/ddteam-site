b5d4e51518e4553c71ea78e48cba92667204c197
WizarD <admin@dd-team.org>
2012-02-19 23:59:04 +0300

    first_commit

---
+++ www/scripts/cufon-replace.js
@@ -0,0 +1,6 @@
+Cufon.replace('h1', { fontFamily: 'Swis721 Th BT' });
+Cufon.replace('.ban_title, .dropcap1', { fontFamily:'Scoder Hand', hover: true});
+Cufon.replace('h3, h2, dt.result-title a, .adminform legend, #users-profile-core legend, #users-profile-custom legend, #member-registration legend', { fontFamily: 'Swis721 Lt BT', hover: true });
+
+
+
